fe73e7497c929d4fc54b05d27a793b99520ff29f
wladislaw353 <wladislaw353@gmail.com>
2023-01-27 03:03:38 +0100

    first commit

---
+++ qr.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('UnicQR.php');
+
+$data = 'https://example.com/';
+
+$options = [
+    'color'    => '#121212',
+    'type'     => 'rect', # cgibrid, circle, rect
+    'capacity' => '3', # 0, 1, 2, 3
+    // 'logo'     => 'logo.svg',
+];
+
+$qr = new UnicQR();
+
+$svg = $qr->generate($data, $options);
+
+file_put_contents('qr.svg', $svg);
+
+// echo $svg;
+
+?>
+
+<img src="qr.svg" width="150px">
